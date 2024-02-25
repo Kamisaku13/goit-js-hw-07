a3c1324d65afa4250ad64eb414a37a74a2961b6b
Kamisaku13 <147526361+Kamisaku13@users.noreply.github.com>
2024-02-25 15:51:46 +0300

    js

---
+++ js/task-1.js
@@ -0,0 +1,12 @@
+const categoriesList = document.getElementById("categories");
+const categoriesItems = categoriesList.querySelectorAll("li.item");
+
+console.log(`Кількість категорій: ${categoriesItems.length}`);
+
+categoriesItems.forEach(category => {
+  const categoryName = category.querySelector("h2").textContent;
+  const categoryItemsCount = category.querySelectorAll("ul > li").length;
+  
+  console.log(`Категорія: ${categoryName}`);
+  console.log(`Кількість елементів: ${categoryItemsCount}`);
+});
